6d982966fec36962fa38d78261ef492dedf4bb2a
agungsaputra19 <119950480+agungsaputra19@users.noreply.github.com>
2022-12-06 22:21:51 +0700

    Add files via upload

---
+++ AgungDwiSaputra_4116/AgungDwiSaputra_4116/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgungDwiSaputra_4116
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Form2 form2 = new Form2();
+			form2.prosesEvent += proses;
+			form2.ShowDialog();
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void proses(int nilaiA, int nilaiB, string operasi, string operasiLabel, float hasil)
+		{
+				listBox1.Items.Add(
+				String.Format($"Hasil {operasiLabel} dari {nilaiA} {operasi} {nilaiB} = ") +
+				String.Format(hasil % 1 == 0 ? "{0:0}" : "{0:0.00}", hasil)
+				);
+		}
+	}
+}
---
+++ AgungDwiSaputra_4116/AgungDwiSaputra_4116/Form2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgungDwiSaputra_4116
+{
+	public partial class Form2 : Form
+	{
+
+		public delegate void Proses(int nilaiA, int nilaiB, string operasi, string operasiLabel, float hasil);
+
+		public event Proses prosesEvent;
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int nilaiA = int.Parse(textBox1.Text);
+			int nilaiB = int.Parse(textBox2.Text);
+			string operasi = "";
+			string operasiLabel = "";
+			float hasil = 0;
+			switch (comboBox1.SelectedIndex)
+			{
+				//penjumlahan
+				case 0:
+					hasil = AgungDwiSaputra_4116.Operasi.Penjumlahan(nilaiA, nilaiB);
+					operasiLabel = "Penjumlahan";
+					operasi = "+";
+					break;
+				//pengurangan
+				case 1:
+					hasil = AgungDwiSaputra_4116.Operasi.Pengurangan(nilaiA, nilaiB);
+					operasiLabel = "Pengurangan";
+					operasi = "-";
+					break;
+				//perkalian
+				case 2:
+					hasil = AgungDwiSaputra_4116.Operasi.Perkalian(nilaiA, nilaiB);
+					operasiLabel = "Perkalian";
+					operasi = "x";
+					break;
+				//pembagian
+				case 3:
+					hasil = AgungDwiSaputra_4116.Operasi.Pembagian(nilaiA, nilaiB);
+					operasiLabel = "Pembagian";
+					operasi = "/";
+					break;
+			}
+			prosesEvent(nilaiA, nilaiB, operasi, operasiLabel, hasil);
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
